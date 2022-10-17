76502f12cf7b162de872b230602a7e513223542b
Ricardo Morin <morinricardo@gmail.com>
2022-10-16 17:26:32 -0700

    Organize webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,11 @@ const HtmlPlugin = require('html-webpack-plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 
-//const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-module.exports = {
+const config = {
   context: path.resolve(__dirname, './src'),
   entry: ['./js/index.js', './style/style.scss'],
   output: {
@@ -16,16 +17,10 @@ module.exports = {
   },
   plugins: [
     new HtmlPlugin({template: 'index.html',inject : true}),
-    // new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery', 'window.jQuery': 'jquery'}),
     new MiniCssExtractPlugin(),
     new CopyWebpackPlugin({patterns: [{from: '../data', to: 'data'}]}),
     //new BundleAnalyzerPlugin()
   ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public')
-    },
-  },
   ignoreWarnings: [
     /only default export is available soon/,
   ],
@@ -60,13 +55,36 @@ module.exports = {
         use: ["source-map-loader"],
       }
     ]
-  },
-  optimization: {
-    concatenateModules: false,
-		minimizer: [new TerserPlugin({
-			terserOptions: {
-				keep_fnames: true
-			}
-		})]
-	}
-};
+  }
+}
+
+module.exports = (env, argv) => {
+  if (argv.mode === 'development') {
+    config.mode = 'development'
+    config.devtool = 'inline-source-map'
+    config.devServer = {
+      static: {
+        directory: path.join(__dirname, 'public')
+      }
+    }
+  }
+
+  if (argv.mode === 'production') {
+    config.mode = 'production'
+    config.optimization = {
+      minimizer: [new TerserPlugin({
+      terserOptions: {
+        keep_fnames: true
+        }
+      }),
+      new CssMinimizerPlugin()]
+    }
+  }
+
+  if (env.analyze === 'yes') {
+    console.log('analyze')
+    config.optimization.concatenateModules = false
+    config.plugins.push(new BundleAnalyzerPlugin())
+  }
+  return config
+}
